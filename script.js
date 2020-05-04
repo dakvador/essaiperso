7f3fb4a8cde8ac8c67f5377adf9e997db1202866
dakvador <dakvador@msn.com>
2020-05-04 05:45:35 +0300

    Vs code commit

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 "use strict";
 
-var myInit ={method:'GET',
-              headers:{'Content-Type': 'application/json'},
-              mode:'cors',
-              cache:'default'};
+// var myInit ={method:'GET',
+//               headers:{'Content-Type': 'application/json'},
+//               mode:'cors',
+//               cache:'default'};
 
-let myRequest = new Request("./myjson.json", myInit); 
-
-
-
-fetch(myRequest)
-    .then(function(res){return res.json})
-    .then(function(data){console.log(data)})
+// let myRequest = new Request("./myjson.json", myInit); 
 
 
 
+// fetch(myRequest)
+//     .then(function(res){return res.json})
+//     .then(function(data){console.log(data)})
 
+
+
+
+
+    var inputfile = document.getElementById('file')
+    inputfile.addEventListener("change",handleFile)
+    function handleFile(files){
+              var myfile = files[0];
+               if(myfile) {console.log(myfile)
+                 let formData = new FormData();
+                formData.append("myfile", myfile);
+                fetch('./download', {method: "POST", body: formData});
+       }
+    }
+
+    function myfunction(){
+        console.log('toto')
+}
